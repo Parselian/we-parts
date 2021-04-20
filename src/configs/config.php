40228132eceee43dbcb5122b7e44ef36fc2612c2
Parselian <parselian.study@yandex.ru>
2021-04-20 12:40:19 +0300

    finished design of delivery page with adaptive

--- src/configs/config.php
+++ src/configs/config.php
@@ -4,5 +4,6 @@
     $phone_format = '+7 (999) 999-99-99';
     $company_name = 'WeParts';
     $email = 'example@ex.ru';
+    $general_addr = 'Московский пр., д.8';
     $site_title = $company_name . ' | Запчасти для техники Apple в СПб';
     $site_link = 'https://we-parts.ru';
